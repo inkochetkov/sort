a5c60196ba39293094af9ffc5ec479027883bc9c
ikochetkov <55381964+inkochetkov@users.noreply.github.com>
2021-07-28 17:48:33 +0300

    t

--- cmd/main.go
+++ cmd/main.go
@@ -9,9 +9,26 @@ type Times struct {
 }
 
 func main() {
-	var calls = []Times{{1200, 1245}, {1200, 1400}, {1230, 1400}}
-	for _, t := range calls {
-		log.Println(t.Start, t.End)
+	var Timeses = []Times{{1200, 1245}, {1200, 1400}, {1730, 1800}}
+	var maxauto = 0
+	for m, _ := range Timeses {
+
+		for j := Timeses[m].Start; j < Timeses[m].End; j++ {
+
+			for i := Timeses[m+1].Start; i < Timeses[m+1].End; i++ {
+
+				if j == i {
+
+					break
+				} else {
+					j = j + 1
+					i = i + 1
+				}
+			}
+		}
+		maxauto = maxauto + 1
+
+		log.Println(maxauto)
 	}
 
 }
